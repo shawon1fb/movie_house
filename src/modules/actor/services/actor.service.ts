200769a3c726a6903fa224af31b47c222c169d7f
shawon1fb <shawon0187@gmail.com>
2022-10-04 23:31:45 +0600

    actor constructor

--- src/modules/actor/services/actor.service.ts
+++ src/modules/actor/services/actor.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { DatabaseEntity } from '../../../common/database/decorators/database.decorator';
+import { Model } from 'mongoose';
+import { ActorDocument, ActorEntity } from '../schemas/actor.schema';
 
 @Injectable()
-export class ActorService {}
+export class ActorService {
+    constructor(
+        @DatabaseEntity(ActorEntity.name)
+        private readonly actorModel: Model<ActorDocument>
+    ) {}
+}
