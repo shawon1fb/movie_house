b9f2a61caf0a4a5e856571ba751dce5289cbdee1
shawon1fb <shawon0187@gmail.com>
2022-10-05 01:47:59 +0600

    broken actor create  1

--- src/modules/actor/controllers/actor.admin.controller.ts
+++ src/modules/actor/controllers/actor.admin.controller.ts
@@ -1,27 +1,93 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+    BadRequestException,
+    Body,
+    Controller,
+    Get,
+    InternalServerErrorException,
+    Post,
+    UploadedFile,
+} from '@nestjs/common';
 import { CreateActorDto } from '../dtos/create.actor.dto';
 import { Response } from '../../../common/response/decorators/response.decorator';
 import { AuthAdminJwtGuard } from '../../../common/auth/decorators/auth.jwt.decorator';
 import { ENUM_AUTH_PERMISSIONS } from '../../../common/auth/constants/auth.enum.permission.constant';
+import { FileRequiredPipe } from '../../../common/file/pipes/file.required.pipe';
+import { FileSizeImagePipe } from '../../../common/file/pipes/file.size.pipe';
+import { FileTypeImagePipe } from '../../../common/file/pipes/file.type.pipe';
+import { IFile } from '../../../common/file/file.interface';
+import { UploadFileSingle } from '../../../common/file/decorators/file.decorator';
+import { ActorService } from '../services/actor.service';
+import { ENUM_ACTOR_STATUS_CODE_ERROR } from '../constans/actor.status-code.constant';
+import { IAwsS3 } from '../../../common/aws/aws.interface';
+import { AwsS3Service } from '../../../common/aws/services/aws.s3.service';
+import { ENUM_ERROR_STATUS_CODE_ERROR } from '../../../common/error/constants/error.status-code.constant';
+import { IResponse } from '../../../common/response/response.interface';
 
 @Controller({
     version: '1',
     path: '/actor',
 })
 export class ActorAdminController {
-    @Get('/hello')
-    async hello() {
-        return 'hello actor admin';
-    }
+    constructor(
+        private readonly actorService: ActorService,
+        private readonly awsService: AwsS3Service
+    ) {}
 
     @Response('actor.create')
+    @UploadFileSingle('file')
     @AuthAdminJwtGuard(
         ENUM_AUTH_PERMISSIONS.ACTOR_READ,
         ENUM_AUTH_PERMISSIONS.ACTOR_CREATE
     )
     @Post('/create')
-    async create(@Body() dto: CreateActorDto) {
-        return dto;
+    async create(
+        @Body() dto: CreateActorDto,
+        @UploadedFile(FileRequiredPipe, FileSizeImagePipe, FileTypeImagePipe)
+        file: IFile
+    ): Promise<IResponse> {
+        const filename: string = file.originalname;
+        const content: Buffer = file.buffer;
+        const mime: string = filename
+            .substring(filename.lastIndexOf('.') + 1, filename.length)
+            .toUpperCase();
+        const path = await this.actorService.createRandomFilename();
+        const exist: boolean = await this.actorService.exists(dto.name);
+
+        if (exist === true) {
+            throw new BadRequestException({
+                statusCode: ENUM_ACTOR_STATUS_CODE_ERROR.ACTOR_EXIST_ERROR,
+                message: 'actor.error.exist',
+            });
+        }
+
+        try {
+            const aws: IAwsS3 = await this.awsService.putItemInBucket(
+                `${path.filename}.${mime}`,
+                content,
+                {
+                    path: `${path.path}/${dto.name}`,
+                }
+            );
+
+            const actor = this.actorService.create(dto, aws);
+            console.log(actor);
+
+            return actor;
+            // return {
+            //     _id: '633c8a2ae96ccea285904e1f',
+            //     name: 'masud',
+            //     gender: 'male',
+            //     about: 'masud',
+            //     createdAt: '2022-10-04T19:30:43.162Z',
+            //     updatedAt: '2022-10-04T19:30:43.162Z',
+            // };
+        } catch (err) {
+            throw new InternalServerErrorException({
+                statusCode: ENUM_ERROR_STATUS_CODE_ERROR.ERROR_UNKNOWN,
+                message: 'http.serverError.internalServerError',
+                error: err.message,
+            });
+        }
     }
 
     @Get('/list')
--- src/modules/actor/services/actor.service.ts
+++ src/modules/actor/services/actor.service.ts
@@ -1,12 +1,58 @@
 import { Injectable } from '@nestjs/common';
 import { DatabaseEntity } from '../../../common/database/decorators/database.decorator';
-import { Model } from 'mongoose';
+import { Model, Types } from 'mongoose';
 import { ActorDocument, ActorEntity } from '../schemas/actor.schema';
+import { HelperStringService } from '../../../common/helper/services/helper.string.service';
+import { ConfigService } from '@nestjs/config';
+import { CreateActorDto } from '../dtos/create.actor.dto';
+import { IAwsS3 } from '../../../common/aws/aws.interface';
 
 @Injectable()
 export class ActorService {
+    private readonly uploadPath: string;
+
     constructor(
         @DatabaseEntity(ActorEntity.name)
-        private readonly actorModel: Model<ActorDocument>
-    ) {}
+        private readonly actorModel: Model<ActorDocument>,
+        private readonly helperStringService: HelperStringService,
+        private readonly configService: ConfigService
+    ) {
+        this.uploadPath = this.configService.get<string>('actor.uploadPath');
+    }
+
+    async createRandomFilename(): Promise<Record<string, any>> {
+        const filename: string = this.helperStringService.random(20);
+
+        return {
+            path: this.uploadPath,
+            filename: filename,
+        };
+    }
+
+    async exists(name: string, _id?: string): Promise<boolean> {
+        const exist = await this.actorModel.exists({
+            name: {
+                $regex: new RegExp(name),
+                $options: 'i',
+            },
+            _id: { $nin: new Types.ObjectId(_id) },
+        });
+
+        return exist ? true : false;
+    }
+
+    async create(dto: CreateActorDto, aws: IAwsS3): Promise<ActorEntity> {
+        const { name, about, gender } = dto;
+
+        const actor: ActorEntity = {
+            name,
+            about,
+            gender,
+            photo: aws,
+        };
+
+        const create: ActorDocument = new this.actorModel(actor);
+        await create.save();
+        return create;
+    }
 }
