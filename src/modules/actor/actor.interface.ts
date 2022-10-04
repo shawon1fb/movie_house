3cca18012ec3757e7789a692b4fd21f6be40ac49
shawon1fb <shawon0187@gmail.com>
2022-10-05 01:47:48 +0600

    broken actor create

---
+++ src/modules/actor/actor.interface.ts
@@ -0,0 +1,3 @@
+import { ActorDocument } from './schemas/actor.schema';
+
+// export type IActorDocument = Omit<ActorDocument, any>;
--- src/modules/actor/serializations/actor.get.serialization.ts
+++ src/modules/actor/serializations/actor.get.serialization.ts
@@ -1,12 +1,14 @@
 import { Exclude, Type } from 'class-transformer';
+import { IAwsS3 } from '../../../common/aws/aws.interface';
 
 export class ActorGetSerialization {
-    @Type(() => String)
-    readonly _id: string;
+    // @Type(() => String)
+    // readonly _id: string;
 
     readonly name: string;
     readonly about: string;
     readonly gender: string;
+    readonly photo?: IAwsS3;
 
     readonly createdAt: Date;
 
