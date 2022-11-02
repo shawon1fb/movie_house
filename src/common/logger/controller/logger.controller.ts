45d30b73ea25a97a0209f683a2167cabcea3602a
shawon1fb <shawon0187@gmail.com>
2022-11-02 23:28:53 +0600

    log files

---
+++ src/common/logger/controller/logger.controller.ts
@@ -0,0 +1,60 @@
+import { Controller, Get, Param, Res, StreamableFile } from '@nestjs/common';
+import { createReadStream, readdirSync, statSync } from 'fs';
+import { join } from 'path';
+import type { Response } from 'express';
+import { RequestParamGuard } from '../../request/decorators/request.decorator';
+import { FileRequestDto } from '../dto/file.request.dto';
+
+@Controller({
+    version: '1',
+    path: '/logger',
+})
+export class LoggerController {
+    @RequestParamGuard(FileRequestDto)
+    @Get('log/http/:name')
+    getFile(
+        @Res({ passthrough: true }) res: Response,
+        @Param('name') filename: string
+    ): StreamableFile {
+        console.log({ filename });
+        const file = createReadStream(
+            join(process.cwd(), 'logs/http/2022-10-31.log')
+        );
+        res.set({
+            'Content-Type': 'application/json',
+            'Content-Disposition': 'attachment; filename="2022-10-24.log"',
+        });
+        return new StreamableFile(file);
+    }
+
+    @Get('/files')
+    fileName() {
+        const getAllFiles = (dirPath, arrayOfFiles) => {
+            const files = readdirSync(dirPath);
+
+            arrayOfFiles = arrayOfFiles || [];
+            const fileList = [];
+
+            files.forEach(function (file) {
+                if (statSync(dirPath + '/' + file).isDirectory()) {
+                    arrayOfFiles = getAllFiles(
+                        dirPath + '/' + file,
+                        arrayOfFiles
+                    );
+                } else {
+                    if (file.endsWith('.log')) {
+                        fileList.push(dirPath + '/' + file);
+                    }
+                }
+            });
+            if (fileList.length !== 0) {
+                // arrayOfFiles[dirPath] = fileList;
+                arrayOfFiles.push(...fileList);
+            }
+
+            return arrayOfFiles;
+        };
+        const result = getAllFiles('logs', []);
+        return { logs: result };
+    }
+}
